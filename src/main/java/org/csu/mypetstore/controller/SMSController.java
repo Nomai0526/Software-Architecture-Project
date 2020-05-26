655ca9833243894448160192c14d7ebccd942d0e
WindReiner <44196436+WindReiner@users.noreply.github.com>
2020-05-26 14:59:58 +0800

    1

--- src/main/java/org/csu/mypetstore/controller/SMSController.java
+++ src/main/java/org/csu/mypetstore/controller/SMSController.java
@@ -24,35 +24,34 @@ public class SMSController {
     public String getSMS()
     {
         String Code = Integer.toString((int)(Math.random()*10000));  //生成四位随机数
-        JSONObject msg = new JSONObject();
-        try {
-            msg.put("code",Code);
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou", "LTAI4FpQ41ZKdmy1bnV2BeVM", "ePSzICHyZrjO76ksnQ6OIhaSe88IA8");
-        IAcsClient client = new DefaultAcsClient(profile);
-
-        CommonRequest request = new CommonRequest();
-        request.setMethod(MethodType.POST);
-        request.setDomain("dysmsapi.aliyuncs.com");
-        request.setVersion("2017-05-25");
-        request.setAction("SendSms");
-        request.putQueryParameter("RegionId", "cn-hangzhou");
-        request.putQueryParameter("PhoneNumbers", "18273119245");
-        request.putQueryParameter("SignName", "MyPetStore");
-        request.putQueryParameter("TemplateCode", "SMS_187745541");
-        request.putQueryParameter("TemplateParam", msg.toString());
-        try {
-            CommonResponse response = client.getCommonResponse(request);
-            System.out.println(response.getData());
-        } catch (ServerException e) {
-            e.printStackTrace();
-        } catch (ClientException e) {
-            e.printStackTrace();
-        }
+//        JSONObject msg = new JSONObject();
+//        try {
+//            msg.put("code",Code);
+//        }
+//        catch (Exception e)
+//        {
+//            e.printStackTrace();
+//        }
+//        IAcsClient client = new DefaultAcsClient(profile);
+//
+//        CommonRequest request = new CommonRequest();
+//        request.setMethod(MethodType.POST);
+//        request.setDomain("dysmsapi.aliyuncs.com");
+//        request.setVersion("2017-05-25");
+//        request.setAction("SendSms");
+//        request.putQueryParameter("RegionId", "cn-hangzhou");
+//        request.putQueryParameter("PhoneNumbers", "18273119245");
+//        request.putQueryParameter("SignName", "MyPetStore");
+//        request.putQueryParameter("TemplateCode", "SMS_187745541");
+//        request.putQueryParameter("TemplateParam", msg.toString());
+//        try {
+//            CommonResponse response = client.getCommonResponse(request);
+//            System.out.println(response.getData());
+//        } catch (ServerException e) {
+//            e.printStackTrace();
+//        } catch (ClientException e) {
+//            e.printStackTrace();
+//        }
         return Code;
     }
 }
